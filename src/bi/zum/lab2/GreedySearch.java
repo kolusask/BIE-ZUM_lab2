44e17e61d8a7bc6deb40323e448feae731306b55
kolusask <19ak97@gmail.com>
2020-03-30 11:45:41 +0200

    implemented greedy search

--- src/bi/zum/lab2/GreedySearch.java
+++ src/bi/zum/lab2/GreedySearch.java
@@ -1,12 +1,14 @@
 package bi.zum.lab2;
 
 import bi.zum.lab1.PathRecSearch;
+import bi.zum.lab2.util.Euclidean;
 import bi.zum.lab2.util.ZumPriorityQueue;
 import cz.cvut.fit.zum.api.AbstractAlgorithm;
 import cz.cvut.fit.zum.api.InformedSearch;
 import cz.cvut.fit.zum.api.Node;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
 import org.openide.util.lookup.ServiceProvider;
 
@@ -32,10 +34,25 @@ public class GreedySearch extends PathRecSearch implements InformedSearch {
         closed = new HashSet<Node>();
         prev = new HashMap<Node, Node>();
         
-        /**
-         * @TODO Implement algorithm
-         *          
-         */
+        open.enqueue(startNode, Euclidean.distance(startNode, endNode));
+        while (!open.isEmpty()) {
+            Node current = open.dequeue();
+            for (Node n : current.expand()) {
+                if (!closed.contains(n) && !open.contains(n)) {
+                    open.enqueue(n, Euclidean.distance(n, endNode));
+                    prev.put(n, current);
+                    if (n.isTarget()) {
+                        List<Node> path = new LinkedList<Node>();
+                        for (current = endNode; current != startNode; current = prev.get(current))
+                            path.add(current);
+
+                        path.add(startNode);
+                        return path;
+                    }
+                }
+            }
+            closed.add(current);
+        }
         
         return null;
     }
